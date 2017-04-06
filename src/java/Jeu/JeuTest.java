38ba5d0ce37245bc8986597e5dd2d65392e3fc4f
moi <kirarmand@gmail.com>
2017-04-06 17:16:39 +0200

    creation Test(en cours)

---
+++ src/java/Jeu/JeuTest.java
@@ -0,0 +1,30 @@
+package Jeu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by moi on 06/04/2017.
+ */
+class JeuTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testActionCasePrise() {
+        Jeu jeu = new Jeu();
+        jeu.init();
+        int j1 = 1;
+        int j2 = 2;
+        int b = 44;
+        assertFalse(jeu.action(j1,b));
+        assertFalse(jeu.action(j2,b));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testInit() {
+        Jeu jeu = new Jeu();
+        jeu.init();
+    }
+
+}
